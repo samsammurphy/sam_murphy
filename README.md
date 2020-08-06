480ea157cff1a0b46faf8a3c5d3a96ff7c1f9eaa
Sam Murphy <samsammurphy@gmail.com>
2020-08-06 14:48:19 +0100

    web.html update

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ### Sam Murphy, PhD
 
 Notes on what you have been up to.
+
+
+#### ideas
+
+make a canvas carousel
+
+https://www.w3schools.com/bootstrap/bootstrap_carousel.asp
--- js/canvas.js
+++ js/canvas.js
@@ -1,10 +1,10 @@
 
-const RADIUS = 20
-const SPEED = 4
-const NUMBALLS = 24
+const RADIUS = 30
+const SPEED = 1
+const NUMBALLS = 10
 
 // canvas DOM element
-const canvas = document.getElementById("canvas"); 
+const canvas = document.getElementById("canvas");
 
 // canvas size and position
 let rect = canvas.getBoundingClientRect();
@@ -30,21 +30,25 @@ window.addEventListener('mousemove',
         mouse.y = event.y - rect.top;
 
         // check mouse is inside the canvas element
-        mouse.inCanvas = (event.x > rect.left) && 
-                         (event.x < rect.right) &&
-                         (event.y < rect.bottom) &&
-                         (event.y > rect.top) 
+        mouse.inCanvas = (event.x > rect.left) &&
+            (event.x < rect.right) &&
+            (event.y < rect.bottom) &&
+            (event.y > rect.top)
 
     }
 )
 
 // resize event listener
-window.addEventListener('resize', function(){
+window.addEventListener('resize', function () {
     // get new canvas position on resize
     rect = canvas.getBoundingClientRect();
     canvas.width = rect.width;
     canvas.height = rect.height;
 })
+
+// refresh button event listener
+const refresh = document.getElementById("canvasRestartButton");
+refresh.addEventListener('click', () => main())
 
 // circle on canvas
 function circle(x, y, r, color) {
@@ -57,114 +61,162 @@ function circle(x, y, r, color) {
 }
 
 // a ball object (for bouncing around in the canvas)
-function Ball(x, y, dx, dy, radius = 10, speed = 4, color = 'cyan') {
-    
+function Ball(location, radius, direction, speed, color = 'cyan') {
+
+    // PROPERTIES
+    // ----------
+
     // clean x and y so that new balls are always totally inside the canvas | avoid edge cases ;)
-    x = _.clamp(x, radius, canvas.width - radius)
-    y = _.clamp(y, radius, canvas.height - radius)
-    
+    x = _.clamp(location.x, radius, canvas.width - radius)
+    y = _.clamp(location.y, radius, canvas.height - radius)
+
     // geometry
     this.x = x;
     this.y = y;
     this.radius = radius
 
     // velocity
+    this.direction = direction
     this.speed = speed
-    this.dx = this.speed * dx
-    this.dy = this.speed * dy
 
     // appearance
     this.colors = ['#086972', '#01a9b4', '#87dfd6', '#fbfd8a'] //https://colorhunt.co/palette/196303
     this.color = this.colors[Math.floor(Math.random() * this.colors.length)]
 
-    // interactivity
-    this.maxRadius = this.radius * 4
+    // interact growth
     this.originalRadius = this.radius
-    this.growthRate = SPEED / 3.14159
+    this.maxRadius = this.radius * 4
+    this.growthRate = 2
 
-    // draw in canvas
+    // interactive speed
+    this.originalSpeed = this.speed
+    this.maxSpeed = 20
+    this.acceleration = 1.1
+    this.deceleration = 0.5
+
+    // METHODS
+    // -------
+
     this.draw = () => circle(this.x, this.y, this.radius, this.color)
 
-    // move the ball
-    this.move = function(){
+    this.move = function () {
 
         // x direction
-        if (this.x + this.radius > canvas.width || 
+        if (this.x + this.radius > canvas.width ||
             this.x - this.radius < 0) {
-            this.dx = -this.dx
+            this.direction.x = -this.direction.x
         }
 
         // y direction
-        if (this.y + this.radius > canvas.height || 
+        if (this.y + this.radius > canvas.height ||
             this.y - this.radius < 0) {
-            this.dy = -this.dy
+            this.direction.y = -this.direction.y
         }
 
-        // velocity
-        this.x += this.dx
-        this.y += this.dy
+        // move ball
+        this.x += this.direction.x * this.speed.x
+        this.y += this.direction.y * this.speed.y
     }
 
-    this.interact = function(){
+    this.grow = () => {
+        if (this.radius < this.maxRadius) {
+            this.radius += this.growthRate;
+        }
+    }
+
+    this.shrink = () => {
+        if (this.radius > this.originalRadius) {
+            this.radius -= this.growthRate;
+        }
+    }
+
+    this.accelerate = () => {
+        if (this.speed.x < this.maxSpeed) {
+            this.speed.x *= this.acceleration
+        }
+        if (this.speed.y < this.maxSpeed) {
+            this.speed.y *= this.acceleration
+        }
+    }
+
+    this.decelerate = () => {
+
+        if (this.speed.x > this.originalSpeed.x) {
+            this.speed.x /= this.deceleration
+        }
+        if (this.speed.y > this.originalSpeed.y) {
+            this.speed.y /= this.deceleration
+        }
+    }
+
+    this.interact = function () {
 
         let mouseDistance = Math.sqrt((mouse.x - this.x) ** 2 + (mouse.y - this.y) ** 2)
 
-        let mouseIsClose = mouseDistance < this.radius
-        let mouseIsFar = mouseDistance > this.radius
+        let mouseOverBall = mouseDistance < this.radius
+        let mouseActive = mouseOverBall && mouse.inCanvas
 
-        // grow balls if mouse is close
-        if (mouseIsClose && mouse.inCanvas) {
-            if (this.radius < this.maxRadius) {
-                this.radius += this.growthRate;
-            }  
+        if (mouseActive) {
+            this.grow()
+            this.accelerate()
         }
 
-        // break balls if mouse is far
-        if (mouseIsFar || !(mouse.inCanvas)) {
-            if (this.radius > this.originalRadius) {
-                this.radius -= this.growthRate;
-            }
+        if (!(mouseActive)) {
+            this.shrink()
+            this.decelerate()
         }
     }
 }
 
 // random balls
-function someBalls(num, radius, speed){
+function someBalls(num, radius, speed) {
 
     let balls = []
     let colors = ["rgba(255,0,0, 0.5)", "rgba(0,255,0, 0.5)", "rgba(0,0,255, 0.5)"]
 
-    for (var i=0; i < num; i++){
+    for (var i = 0; i < num; i++) {
 
-        // random start location 
-        let x = Math.random() * canvas.width 
-        let y = Math.random() * canvas.height
+        let random_location = {
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height
+        }
 
-        // random direction
-        let dx = Math.random() - 0.5
-        let dy = Math.random() - 0.5
+        let random_direction = {
+            x: (Math.random() > 0.5) ? -1 : 1,
+            y: (Math.random() > 0.5) ? -1 : 1
+        }
 
-        balls.push(new Ball(x, y, dx, dy, radius=radius, speed=speed, color=colors[i]))
+        let random_speed = {
+            x: Math.max(SPEED * Math.random(), SPEED / 4),
+            y: Math.max(SPEED * Math.random(), SPEED / 4)
+        }
+
+        balls.push(new Ball(random_location, radius, random_direction, random_speed, color = colors[i]))
     }
 
     return balls
 }
 
-// get some balls
-let balls = someBalls(NUMBALLS, RADIUS, SPEED)
+function main() {
 
-// a circle that bounces around the screen
-function play(){
-    requestAnimationFrame(play);
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    // get some balls
+    let balls = someBalls(NUMBALLS, RADIUS, SPEED)
 
-    for (var i=0; i<balls.length; i++){
-        balls[i].draw()
-        balls[i].move()
-        balls[i].interact()
+    // a circle that bounces around the screen
+    function play() {
+        requestAnimationFrame(play);
+        c.clearRect(0, 0, canvas.width, canvas.height)
+
+        for (var i = 0; i < balls.length; i++) {
+            balls[i].draw()
+            balls[i].move()
+            balls[i].interact()
+        }
+
     }
 
+    // run animation
+    play()
 }
 
-// run animation
-play()
+main()
